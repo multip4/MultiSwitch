10c20b382a6a4197cce539c3296045459c442896
Seokwon Jang <imsoboy2@gmail.com>
2017-10-12 18:44:44 +0900

    Update send.py

--- Software/u_gateway/send.py
+++ Software/u_gateway/send.py
@@ -5,9 +5,11 @@ import socket
 import random
 import struct
 from scapy.all import *
-
+// Dot11 packet header gen
 packet = Dot11()/ LLC()/ SNAP() / IP(dst= "10.0.2.10") / UDP() / "hi"
-#packet = Ether()/IP(dst="10.0.2.10") /UDP()/"hi"
+
+// Ether packet header gen
+packet2 = Ether()/IP(dst="10.0.2.10") /UDP()/"hi"
 hexdump (packet)
 packet.show2()
 sendp(packet,count=1, inter=1, iface="veth0") 
