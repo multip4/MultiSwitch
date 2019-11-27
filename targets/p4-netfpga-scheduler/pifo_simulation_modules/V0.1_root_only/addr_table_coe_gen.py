32d36157e96f9457823ef0066faf15114ed624ba
Zhenguo Cui <quejinkook@gmail.com>
2019-11-27 11:39:14 +0900

    add coe generator

---
+++ targets/p4-netfpga-scheduler/pifo_simulation_modules/V0.1_root_only/addr_table_coe_gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+output_file = open(sys.argv[2], "w")
+output_file.write("memory_initialization_radix=10;\n")
+output_file.write("memory_initialization_vector=\n")
+
+depth = int(sys.argv[1])
+
+
+
+for i in range(1,depth):
+	# output_file.write(str(4096-i) + ","+"\n")
+	output_file.write(str(i) + ","+"\n")
+output_file.write(str(depth-1))
+output_file.write("\n")
+# output_file.write("4095,\n")
+
+# output_file.write("1;\n")
