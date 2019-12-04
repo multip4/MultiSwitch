210c4db5d1f537b274711ae5e9490fa70a51c909
Zhenguo Cui <quejinkook@gmail.com>
2019-12-04 23:42:42 +0900

    Add synthesis result files to reduce synth time.
    need to add following tcl script at simple_sume_switch.tcl
    
    "file copy $::env(P4_PROJECT_PIFO_MODULES_DIR)/ project/simple_sume_switch.runs/"
    
    Signed-off-by: Zhenguo Cui <quejinkook@gmail.com>

--- targets/p4-netfpga-scheduler/pifo_simulation_modules/V0.1_root_only/settings.sh
+++ targets/p4-netfpga-scheduler/pifo_simulation_modules/V0.1_root_only/settings.sh
@@ -1,2 +1,3 @@
 export P4_PROJECT_PIFO_MODULES_DIR=/home/jkchoi/Documents/git/MultiSwitch/targets/p4-netfpga-scheduler/pifo_simulation_modules/V0.1_root_only
 export P4_PROJECT_DEMO_TCL=p4_pifo_demo
+export SUME_SYNTH_DIR=$P4_PROJECT_PIFO_MODULES_DIR/../../vivado_projects/simple_sume_switch.runs
